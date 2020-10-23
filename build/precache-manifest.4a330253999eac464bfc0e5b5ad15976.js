02675175e7aaa294b027c0a11e17158624c31050
jarabik8810 <alexjarabik@gmail.com>
2020-10-23 17:08:19 +0200

    add base file structure

---
+++ build/precache-manifest.4a330253999eac464bfc0e5b5ad15976.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2462fd8498c887ff970ebad4be740e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c57081c5cfd662149a",
+    "url": "/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a86638089b4d300d0265",
+    "url": "/static/css/main.6c21c107.chunk.css"
+  },
+  {
+    "revision": "c9c57081c5cfd662149a",
+    "url": "/static/js/2.25058f8c.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/static/js/2.25058f8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86638089b4d300d0265",
+    "url": "/static/js/main.09a2a0bd.chunk.js"
+  },
+  {
+    "revision": "2e075117383e7b156060",
+    "url": "/static/js/runtime-main.2f0bb968.js"
+  }
+]);
